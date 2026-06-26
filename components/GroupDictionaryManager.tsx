6a19c4fdf177d9022b9183963c727c7b6a0bd81a
Christian <christian.knopke@gmail.com>
2026-06-26 12:12:56 +0000

    Tab "Gruppen-Manager" → "Abteilungs-Wörterbücher"

--- components/GroupDictionaryManager.tsx
+++ components/GroupDictionaryManager.tsx
@@ -593,7 +593,7 @@ export default function GroupDictionaryManager() {
         </div>
       )}
 
-      {/* Tabs: Gruppen-Manager / Alle Benutzerwörterbücher (nur root) */}
+      {/* Tabs: Abteilungs-Wörterbücher / Alle Benutzerwörterbücher (nur root) */}
       {isRoot && (
         <div className="flex gap-1 border-b dark:border-gray-700 pb-0">
           <button
@@ -605,7 +605,7 @@ export default function GroupDictionaryManager() {
                 : 'text-gray-500 hover:text-gray-700 dark:hover:text-gray-300 border-transparent'
             }`}
           >
-            Gruppen-Manager
+            Abteilungs-Wörterbücher
           </button>
           <button
             type="button"
